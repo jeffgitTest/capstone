466056a9f57f66e83ebb66a8ba397741585f0631
natsumendoza <mendozalaxus@yahoo.com>
2017-03-09 12:41:28 +0800

    updates

--- admin/accounting/income.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-	include "../include/connectdb.php";
-
-
- ?>
--- admin/template/sidebar.php
+++ admin/template/sidebar.php
@@ -26,8 +26,8 @@
               <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-caret-square-o-down"></i>ACCOUNTING<b class="caret"></b></a>
               <ul class="dropdown-menu">
                 <li><a href="./accounting/sales.php">Sales</a></li>
-                <li><a href="./accounting/income.php">Income</a></li>
                 <li><a href="./accounting/expenses.php">Expenses</a></li>
+                <li><a href="./accounting/financialstatement.php">Financial Statement</a></li>
               </ul>
             </li>
            <li class="dropdown">
