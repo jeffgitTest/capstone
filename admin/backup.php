1496140d4e33ee42ec7befb660634ab2cacace5b
natsumendoza <mendozalaxus@yahoo.com>
2017-03-22 16:02:31 +0800

    updates

--- admin/backup.php
+++ admin/backup.php
@@ -1,60 +1,5 @@
 <!DOCTYPE html>
-<?php 
-    if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['databasename'])) {
-        $username = trim($_POST['username']);
-        $password = trim($_POST['password']);
-        $databasename = trim($_POST['databasename']);
-        $backupRestore = $_POST['backupRestore'];
- 
-        if ($backupRestore == 'backup'){        
-            $data = $_POST['data'];
-            $now = str_replace(":", "", date("Y-m-d H:i:s"));
-            $outputfilename = $databasename . '-' . $now . '.sql';
-            $outputfilename = str_replace(" ", "-", $outputfilename);
- 
-            //Dump the MySQL database
-            if ($data == "wd"){
-                //With data
-                exec('mysqldump -u '. $username .' -p'. $password .' '. $databasename .' > '. $outputfilename);
-            }
-            else{
-                //Without data
-                exec('mysqldump --no-data  -u '. $username .' -p'. $password .' '. $databasename .' > '. $outputfilename);
-            }   
- 
-            //Download the database file
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename='.basename($outputfilename));
-            header('Content-Transfer-Encoding: binary');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($outputfilename));
-            ob_clean();
-            flush();
-            readfile($outputfilename);
-         
-            //After download remove the file from server
-            exec('rm ' . $outputfilename);  
- 
-        }
-        else{//Restore the database
- 
-            $target_path = getcwd();
-            $databasefilename = $_FILES["databasefile"]["name"];
- 
-            //Upload the database file to current working directory
-            move_uploaded_file($_FILES["databasefile"]["tmp_name"], $target_path . '/' . $databasefilename);
- 
-            //Restore the database          
-            exec('mysql -u '. $username .' -p'. $password .' '. $databasename .' < '. $databasefilename);
-             
-            //Remove the file from server
-            exec('rm ' . $databasefilename);
-        }
-    }
-?>
+
 <html>
     <head>
         <title>MySQL database backup</title>
@@ -69,9 +14,6 @@
                 var databasename = $.trim($("#databasename").val());
                 if (username == ""){
                     alert("Plsease enter mysql username.");return false;
-                }
-                else if (password == ""){
-                    alert("Plsease enter mysql password.");return false;
                 }
                 else if (databasename == ""){
                     alert("Plsease enter mysql database name.");return false;
@@ -114,17 +56,17 @@
             <tr>
                 <td>MySQL username*</td>
                 <td>:</td>
-                <td><input type="text" id="username" name="username" value="a2333010_root"/></td>
+                <td><input type="text" id="username" name="username" value="root"/></td>
             </tr>
             <tr>
                 <td>MySQL password*</td>
                 <td>:</td>
-                <td><input type="text" id="password" name="password" value="klangklang1"/></td>
+                <td><input type="text" id="password" name="password" value="root"/></td>
             </tr>
             <tr>
                 <td>MySQL database name*</td>
                 <td>:</td>
-                <td><input type="text" id="databasename" name="databasename" value="a2333010_cpstn"/></td>
+                <td><input type="text" id="databasename" name="databasename" value="capstone"/></td>
             </tr>
             <tr>
                 <td colspan="2">Backup <input type="radio" name="backupRestore" id="backup" value="backup" checked="true" onclick="showHide(this.id);" /></td>
@@ -147,10 +89,53 @@
         </form>
 		
 		<?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'capstone504db';
+
+if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['databasename'])) {
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+        $databasename = trim($_POST['databasename']);
+        $backupRestore = $_POST['backupRestore'];
+
+
+        if ($backupRestore == 'backup'){        
+            $data = $_POST['data'];
+            $now = str_replace(":", "", date("Y-m-d H:i:s"));
+            $outputfilename = $databasename . '-' . $now . '.sql';
+            $outputfilename = str_replace(" ", "-", $outputfilename);
+ 
+            //Dump the MySQL database
+            if ($data == "wd"){
+                //With data
+                exec('mysqldump -u '. $username .' -p'. $password .' '. $databasename .' > '. $outputfilename);
+            }
+            else{
+                //Without data
+                exec('mysqldump --no-data  -u '. $username .' -p'. $password .' '. $databasename .' > '. $outputfilename);
+            }   
+ 
+            
+         
+            //After download remove the file from server
+            exec('rm ' . $outputfilename);  
+ 
+        }
+        else{//Restore the database
+ 
+            $target_path = getcwd();
+            $databasefilename = $_FILES["databasefile"]["name"];
+ 
+            //Upload the database file to current working directory
+            move_uploaded_file($_FILES["databasefile"]["tmp_name"], $target_path . '/' . $databasefilename);
+ 
+            //Restore the database          
+            exec('mysql -u '. $username .' -p'. $password .' '. $databasename .' < '. $databasefilename);
+             
+            
+        }
+
+
+    }
+
 
 function backup_tables($host,$user,$pass,$name,$tables = '*')
 {
@@ -202,10 +187,38 @@ function backup_tables($host,$user,$pass,$name,$tables = '*')
         $return.="\n\n\n";
     }
 
+    $now = str_replace(":", "", date("Y-m-d H:i:s"));
+    $outputfilename = $name . '-' . $now . '.sql';
+    $outputfilename = str_replace(" ", "-", $outputfilename);
+
+    //save file
+    $handle = fopen($outputfilename,'w+');
+    fwrite($handle,$return);
+    fclose($handle);
+
+    //Download the database file
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($outputfilename));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($outputfilename));
+
+    ob_clean();
+    flush();
+    readfile($outputfilename);
+
+    //Remove the file from server
+    exec('rm ' . $outputfilename);
+
     //save file
     $handle = fopen('backup_temp/db-backup-'.time().'-'.(md5(implode(',',$tables))).'.sql','w+');
     fwrite($handle,$return);
     fclose($handle);
+
+    return $return;
 }
 
 backup_tables($dbhost,$dbuser,$dbpass,$dbname);
